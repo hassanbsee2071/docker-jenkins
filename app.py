a06a8585c3605303b99e620b3ae4a6fcc49b4c05
hassanbsee2071 <hassan.bsee2071@iiu.edu.pk>
2020-10-07 00:19:04 +0500

    app

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+    return "Hello World! This is wonderfull Testing"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run(host= '0.0.0.0')                 # run the flask app
