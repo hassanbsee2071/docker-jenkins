2d72c584a3ce39ffb00fbb062cd77a0427842241
hassanbsee2071 <hassan.bsee2071@iiu.edu.pk>
2021-10-05 16:41:13 +0500

    apps2021

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello World! Allah will help me inshallah"         # which returns "hello world"
+    return "Hello World! Allah will help me inshallah.He is the best"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run(host= '0.0.0.0')                 # run the flask app
